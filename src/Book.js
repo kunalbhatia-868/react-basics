b5f50828b4ca9cf2f2e69cf2b1967478fa909aa5
kunal <kunalbhatia868@gmail.com>
2021-09-13 17:35:42 +0530

    basic formatting added

--- src/Book.js
+++ src/Book.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Book = (props) => {
   //attribute,event handler
   //onClick and on mouse Hover
 
-  const clickHandler=()=>{
-    alert('Hello World');
-  }
-  const complexExample=(author)=>{
-     console.log(author);
-  }
+  const clickHandler = () => {
+    alert("Hello World");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
 
-  console.log(props)
+  console.log(props);
   const { img, title, author } = props;
   return (
-    <article className="book" onMouseOver={()=>console.log(title)}>
-      <img src={img} alt=""/>
-      <h1 onClick={()=>console.log({title})}>{title}</h1>
+    <article className="book" onMouseOver={() => console.log(title)}>
+      <img src={img} alt="" />
+      <h1 onClick={() => console.log({ title })}>{title}</h1>
       <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>Reference</button>
-      <button type="button" onClick={()=>complexExample(author)}>more complex examplee</button>
-
+      <button type="button" onClick={clickHandler}>
+        Reference
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex examplee
+      </button>
     </article>
   );
 };
 
-export default Book
+export default Book;
--- src/books.js
+++ src/books.js
@@ -1,20 +1,20 @@
 export const books = [
-  { 
-    id:1,    
+  {
+    id: 1,
     title: "The Psychology of Money",
     author: "Morgan Housel",
     img: "https://images-eu.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg",
   },
   {
-    id:2,  
+    id: 2,
     title: "The Japanese secret to a long and happy life",
     author: "Héctor García",
     img: "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg",
   },
   {
-    id:3, 
-    title:'Rich Dad Poor Dad',
-    author:'Robert T. Kiyosaki',
-    img:'https://images-eu.ssl-images-amazon.com/images/I/81dQwQlmAXL._AC_UL200_SR200,200_.jpg',
-  }
+    id: 3,
+    title: "Rich Dad Poor Dad",
+    author: "Robert T. Kiyosaki",
+    img: "https://images-eu.ssl-images-amazon.com/images/I/81dQwQlmAXL._AC_UL200_SR200,200_.jpg",
+  },
 ];
